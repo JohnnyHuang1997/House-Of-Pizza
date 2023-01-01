382993a6772b1dd3890d61e15bec8f8c221a6dd4
Johnny Huang <johnnyhuang15@hotmail.com>
2022-12-31 23:01:42 -0500

    HomePage component

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,13 +1,55 @@
 import banner from '../assets/shokugeki.jpeg';
 import styled from 'styled-components';
+import { useEffect, useState } from 'react';
 
 const HomePage = () => {
+	const [pizzas, setPizzas] = useState(null);
+
+	useEffect(() => {
+		fetch('/pizzas')
+			.then((res) => res.json())
+			.then((data) => {
+				setPizzas(data.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
+
 	return (
-		<div className='flex flex-col'>
-			<div className='flex justify-center items-center'>
-				<Banner src={banner} alt='banner' />
-			</div>
-		</div>
+		<>
+			{!pizzas ? (
+				<h1>Loading...</h1>
+			) : (
+				<div className='flex flex-col justify-center items-center'>
+					<div className='flex justify-center items-center flex-col '>
+						<Banner src={banner} alt='banner' />
+						<h2 className='shadow-lg text-5xl my-3 font-serif text-gray-900 uppercase'>
+							House of pizza
+						</h2>
+						<div className='grid grid-cols-3 justify-center items-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-10'>
+							{pizzas.map((pizzaContent) => {
+								return (
+									<div
+										className='py-5 px-5 flex flex-col justify-center items-center text-center bg-gray-900 rounded-xl'
+										key={pizzaContent.id}
+									>
+										<Image src={pizzaContent.src} alt='pizza content' />
+										<p className='text-2xl uppercase text-white'>
+											{pizzaContent.name}
+											<br />
+											{pizzaContent.description}
+											<br />
+											{pizzaContent.topping}
+										</p>
+									</div>
+								);
+							})}
+						</div>
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
@@ -16,5 +58,11 @@ export default HomePage;
 const Banner = styled.img`
 	object-fit: contain;
 	height: 30vh;
-	width: 1000px;
+	width: 100%;
+`;
+
+const Image = styled.img`
+	width: 800px;
+	aspect-ratio: 3/2;
+	border-radius: 20px;
 `;
